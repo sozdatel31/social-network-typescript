d068d2205dbca2d184eacd762cbf3fdf711dc00c
sozdatel31 <pavlik.gerasim@gmail.com>
2021-03-02 22:42:00 +0300

    end 11 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,32 @@ import './App.css';
 function App() {
     return (
         <div className="app-wrapper">
-            <header>
+            <header className="header">
                 <img src="https://i.pinimg.com/originals/33/b8/69/33b869f90619e81763dbf1fccc896d8d.jpg"/>
             </header>
-            <nav>
-                <div>Profile</div>
-                <div>Messages</div>
-                <div></div>
-                <div></div>
-                <div></div>
+            <nav className="nav">
+                <div>
+                    <a>Profile</a>
+
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
             </nav>
-            <div>
-                Main content
+            <div className="content">
+                <div><img src="https://www.meme-arsenal.com/memes/50569ac974c29121ff9075e45a334942.jpg"/></div>
+                <div><img src="https://avatarko.ru/img/kartinka/33/multfilm_lyagushka_32117.jpg"/></div>
             </div>
+
         </div>
     );
 }
