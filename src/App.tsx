4027e86828688c19035cf751104ea90301567b82
sozdatel31 <pavlik.gerasim@gmail.com>
2021-03-03 19:38:40 +0300

    create components, end 12 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from "./Components/Header";
+import Navbar from "./Components/Nav";
+import Profile from "./Components/Profile";
 
 function App() {
     return (
         <div className="app-wrapper">
-            <header className="header">
-                <img src="https://i.pinimg.com/originals/33/b8/69/33b869f90619e81763dbf1fccc896d8d.jpg"/>
-            </header>
-            <nav className="nav">
-                <div>
-                    <a>Profile</a>
-
-                </div>
-                <div>
-                    <a>Messages</a>
-                </div>
-                <div>
-                    <a>News</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-                <div>
-                    <a>Settings</a>
-                </div>
-            </nav>
-            <div className="content">
-                <div><img src="https://www.meme-arsenal.com/memes/50569ac974c29121ff9075e45a334942.jpg"/></div>
-                <div><img src="https://avatarko.ru/img/kartinka/33/multfilm_lyagushka_32117.jpg"/></div>
-            </div>
-
+            <Header />
+            <Navbar />
+            <Profile />
         </div>
     );
 }
---
+++ src/Components/Profile.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Profile() {
+    return (<div className="content">
+        <div><img src="https://www.meme-arsenal.com/memes/50569ac974c29121ff9075e45a334942.jpg"/></div>
+        <div><img src="https://avatarko.ru/img/kartinka/33/multfilm_lyagushka_32117.jpg"/></div>
+        <div>ava + description</div>
+        <div>
+            My posts
+        </div>
+        <div>New post</div>
+        <div>post 1</div>
+        <div>post 2</div>
+    </div>)
+}
+export default Profile;
