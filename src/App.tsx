8a7bf613276d0ae5dcc232145b242482b5032573
sozdatel31 <pavlik.gerasim@gmail.com>
2021-03-09 23:04:09 +0300

    end 18  lesson

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import './App.css';
 import Header from "./Components/Header/Header";
 import Navbar from "./Components/Nav/Nav";
 import Profile from "./Components/Profile/Profile";
+import Dialogs from "./Components/Dialogs/Dialogs";
 
 function App() {
     return (
         <div className="app-wrapper">
             <Header/>
             <Navbar/>
-            <Profile/>
+            <div className="app-wrapper-content">
+            <Dialogs/>
+            </div>
+            {/*<Profile/>*/}
+
         </div>
     );
 }
--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -3,7 +3,7 @@ import s from "./Profile.module.css"
 import MyPosts from "./MyPosts/MyPosts";
 
 function Profile() {
-    return (<div className={s.content}>
+    return(<div>
         <div><img src="https://www.meme-arsenal.com/memes/50569ac974c29121ff9075e45a334942.jpg"/></div>
         <div><img src="https://avatarko.ru/img/kartinka/33/multfilm_lyagushka_32117.jpg"/></div>
         <div>ava + description</div>
