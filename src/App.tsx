74ee25438342321d8187c2ac962e53468ee72e1b
sozdatel31 <pavlik.gerasim@gmail.com>
2021-03-10 22:37:21 +0300

    end 20 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,27 @@ import Header from "./Components/Header/Header";
 import Navbar from "./Components/Nav/Nav";
 import Profile from "./Components/Profile/Profile";
 import Dialogs from "./Components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from 'react-router-dom';
+import News from "./Components/News/News";
+import Music from "./Components/Music/Music";
+import Settings from "./Components/Settings/Settings";
 
 function App() {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            <div className="app-wrapper-content">
-            <Dialogs/>
-            </div>
-            {/*<Profile/>*/}
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className="app-wrapper-content">
+                    <Route path="/dialogs" component={Dialogs}/>
+                    <Route path="/profile" component={Profile}/>
+                    <Route path="/news" component={News}/>
+                    <Route path="/music" component={Music}/>
+                    <Route path="/settings" component={Settings}/>
+                </div>
 
-        </div>
+            </div>
+        </BrowserRouter>
     );
 }
 
--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -4,8 +4,8 @@ import MyPosts from "./MyPosts/MyPosts";
 
 function Profile() {
     return(<div>
-        <div><img src="https://www.meme-arsenal.com/memes/50569ac974c29121ff9075e45a334942.jpg"/></div>
-        <div><img src="https://avatarko.ru/img/kartinka/33/multfilm_lyagushka_32117.jpg"/></div>
+        <div className={s.ava}><img src="https://www.meme-arsenal.com/memes/50569ac974c29121ff9075e45a334942.jpg"/></div>
+        <div className={s.fon}><img src="https://avatarko.ru/img/kartinka/33/multfilm_lyagushka_32117.jpg"/></div>
         <div>ava + description</div>
         <MyPosts/>
     </div>)
