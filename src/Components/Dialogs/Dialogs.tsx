a566445d4a08153d1d4effad2132a4b5eaf6aff6
sozdatel31 <pavlik.gerasim@gmail.com>
2021-03-10 23:45:07 +0300

    end 21 lesson

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -3,8 +3,21 @@ import s from "./Dialogs.module.css"
 
 function Dialogs() {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div>Nadya</div>
+                <div>Kolya</div>
+                <div>Antony</div>
+                <div>Ivan</div>
+                <div>Victor</div>
+            </div>
+            <div className={s.message}>
+                <div>Yo</div>
+                <div>Hello, Kaban?</div>
+                <div>Sosi sosisky</div>
+                <div>Ssakroy ebalo</div>
+                <div>Meowwwwwwww</div>
+            </div>
         </div>
     )
 }
