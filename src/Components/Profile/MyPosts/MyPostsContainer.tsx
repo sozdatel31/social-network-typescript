9120eea21ebacc4e923ec160878fa308dcf8ad37
sozdatel31 <pavlik.gerasim@gmail.com>
2021-05-11 22:51:12 +0300

    end 59 lesson. +ProfilePage +typeOfProfilePage

--- src/Components/Profile/MyPosts/MyPostsContainer.tsx
+++ src/Components/Profile/MyPosts/MyPostsContainer.tsx
@@ -1,50 +1,18 @@
 import React from "react";
-import s from "./MyPosts.module.css"
-import Post from "./Post/Post";
 
 import {addPostAC, UpdateTextPostAC} from "../../../Redux/profile-reducer";
-import {AppStateType, store} from "../../../Redux/redux-store";
+import {AppStateType} from "../../../Redux/redux-store";
 import MyPosts from "./MyPosts";
-import {addMessageAC, dialogsPageTypes, UpdateMessagePostAC} from "../../../Redux/dialogs-reducer";
 import {Dispatch} from "redux";
 import {connect} from "react-redux";
-import Dialogs from "../../Dialogs/Dialogs";
 import {PostType} from "../../../Redux/store";
+
 export type profilePageType = {
     postData: Array<PostType>
     changePostText: string
 }
 
-type myPostsPropsType = {
-    store: typeof store
-    // postData: Array<PostType>
-    //  addPost: (message: string) => void
-    // changePostText: string
-    // //updateNewPostText: (newText: string) => void
-    // dispatch: (action: ActionType) => void
-}
 
-// function MyPostsСontainer(props: myPostsPropsType) {
-//     const state = props.store.getState();
-//
-//     const onAddPost = (currentText: string) => {
-//         props.store.dispatch(addPostAC(currentText));
-//     }
-//     const updateNewPostText = (text: string) => {
-//         const action = UpdateTextPostAC(text);
-//         props.store.dispatch(action)
-//     }
-//
-//     return (
-//         <div>
-//             <MyPosts addPost={onAddPost}
-//                      updateNewPostText={updateNewPostText}
-//                      postData={state.profilePage.postData}
-//                      changePostText={state.profilePage.changePostText}
-//             />
-//         </div>
-//     )
-// }
 type MapStateToPropsType = {
     profilePage: profilePageType
 }
